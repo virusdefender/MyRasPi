46ad00e4d42cea928cd872ca1ffe39a5291bc798
sndnvaps <sndnvaps@gmail.com>
2016-02-03 01:08:49 +0800

    Add rpi_cpu_temp for mysql database  * Support upload rpi_cpu_temp to machtalk.net
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- machtalk/dht22/TempRecord/create-table-mysql.sql
+++ machtalk/dht22/TempRecord/create-table-mysql.sql
@@ -9,10 +9,14 @@ CREATE TABLE pi_temps (
 	name varchar(30) NOT NULL DEFAULT 'DHT.RPI_RECORDER' ,
 	temp varchar(5) NOT NULL DEFAULT '0.0' COMMENT 'DHT.Temperature',
 	humi varchar(5) NOT NULL DEFAULT '0.0' COMMENT 'DHT.Humidity',
+	rpi_cpu_temp varchar(6) NOT NULL DEFAULT '0.0' COMMENT 'RPI.CPU.Temperature',
+	update_time datetime NOT NULL COMMENT 'update time',
 	PRIMARY KEY (id)
 ) charset=utf8;
-
+/*
 ALTER TABLE pi_temps add update_time datetime NOT NULL COMMENT 'update time';
+ALTER TABLE pi_temps add pi_cpu_temp varchar(6) NOT NULL DEFAULT '0.0' COMMENT 'RPI.CPU.Temperature';
+*/
 
 
 
--- machtalk/dht22/machtalk_dht22.cpp
+++ machtalk/dht22/machtalk_dht22.cpp
@@ -23,6 +23,7 @@
 extern "C" {
 #include "dht22.h"
 #include "machtalk.h"
+#include "rpi_cpu_temp.h"
 }
 
 using namespace std;
@@ -31,17 +32,25 @@ std::mutex g_mutex;
 //#define api_token "95345cb2cbc945da978afb4441da6bcf"
 
 #ifdef SUPPORT_MYSQL_DB
+using namespace  RPI_TEMP_RECORD;
+
 std::string float2string(float val) {
 	stringstream ss(stringstream::in | stringstream::out);
 	ss << val;
 	std::string out = ss.str();
 	return out;
 }
-
+/*
 void Insert(string temp,string hum) {
 	RPI_TEMP_RECORD::Insert(temp,hum);
 	cout<< "inster into table" << endl;
 }
+*/
+void Insert(string temp,string hum,string rpi_cpu_temp) {
+	insert(temp,hum,rpi_cpu_temp);
+	cout<< "inster into table" << endl;
+}
+
 #endif 
 
 
@@ -56,17 +65,21 @@ void Machtalk_Post(string api_token,string device_id, string device_value_id, st
 int main() {
     float temp;
     float hum;
+    float rpi_cpu_temp;
     string api_token = "95345cb2cbc945da978afb4441da6bcf";
     GetDht22_data(&temp, &hum);
+    rpi_cpu_temp = Get_rpi_temp();
 #ifdef SUPPORT_MYSQL_DB
-	Insert(float2string(temp),float2string(hum));
+	Insert(float2string(temp),float2string(hum),float2string(rpi_cpu_temp));
 #endif 
      std::thread t1; //t1 is not a thread 
      std::thread t2(Machtalk_Post,api_token,"df104baddce24fd0a5e976c90fc07df3","1","1",temp,3);
      std::thread t3(Machtalk_Post,api_token,"df104baddce24fd0a5e976c90fc07df3","2","1",hum,3);
+     std::thread t4(Machtalk_Post,api_token,"df104baddce24fd0a5e976c90fc07df3","3","1",rpi_cpu_temp,3);
     
      t2.join();
      t3.join();   
+     t4.join();
    return 0;
 }
 

1287cabfb58806c6b700b8caa115ba5f407b287a
sndnvaps <sndnvaps@gmail.com>
2016-02-04 23:37:57 +0800

    update README.md && update machtalk_dht22.cpp
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- machtalk/dht22/machtalk_dht22.cpp
+++ machtalk/dht22/machtalk_dht22.cpp
@@ -66,16 +66,17 @@ int main() {
     float temp;
     float hum;
     float rpi_cpu_temp;
-    string api_token = "95345cb2cbc945da978afb4441da6bcf";
+    string api_token = "95345cb2cbc945da978afb4441da6bcf"; //Replace with you api_token
+    string device_id  = "df104baddce24fd0a5e976c90fc07df3";  //Replace with you device_id 
     GetDht22_data(&temp, &hum);
     rpi_cpu_temp = Get_rpi_temp();
 #ifdef SUPPORT_MYSQL_DB
 	Insert(float2string(temp),float2string(hum),float2string(rpi_cpu_temp));
 #endif 
      std::thread t1; //t1 is not a thread 
-     std::thread t2(Machtalk_Post,api_token,"df104baddce24fd0a5e976c90fc07df3","1","1",temp,3);
-     std::thread t3(Machtalk_Post,api_token,"df104baddce24fd0a5e976c90fc07df3","2","1",hum,3);
-     std::thread t4(Machtalk_Post,api_token,"df104baddce24fd0a5e976c90fc07df3","3","1",rpi_cpu_temp,3);
+     std::thread t2(Machtalk_Post,api_token,device_id,"1","1",temp,3);
+     std::thread t3(Machtalk_Post,api_token,device_id,"2","1",hum,3);
+     std::thread t4(Machtalk_Post,api_token,device_id,"3","1",rpi_cpu_temp,3);
     
      t2.join();
      t3.join();   

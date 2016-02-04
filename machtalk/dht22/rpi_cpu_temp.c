850d9532be12ab7c598c46fd7e0d5734a694fe9a
sndnvaps <sndnvaps@gmail.com>
2016-02-04 23:10:15 +0800

    machtalk/dht22/rpi_cpu_temp.c: *Return %2.2f value of cpu_temp fix upload func of machtalk_upload()
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- machtalk/dht22/rpi_cpu_temp.c
+++ machtalk/dht22/rpi_cpu_temp.c
@@ -7,11 +7,13 @@
 
 float Get_rpi_temp() {
    int fd, size;
+  char rpi_temp[6] = { };
    char buffer[15] = { };
    fd = open("/sys/class/thermal/thermal_zone0/temp", O_RDONLY);
    size = read(fd,buffer, sizeof(buffer));
    close(fd);
    float tf = atof(buffer);
    printf("RasPi Cpu Tempture = %2.2f\n", tf/1000);
-   return (tf/1000); 
+   sprintf(rpi_temp,"%2.2f",tf/1000);
+   return atof(rpi_temp);
 }
